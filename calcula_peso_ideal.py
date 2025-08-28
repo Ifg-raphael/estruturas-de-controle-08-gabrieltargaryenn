0d87fa10c807ccf584ff579fb40115393e0e1370
gabrieltargaryenn <gabrielalvestargaryen@gmail.com>
2025-08-28 19:56:08 -0300

    atividade finalizada

--- calcula_peso_ideal.py
+++ calcula_peso_ideal.py
@@ -1,20 +1,11 @@
 # Sua solução aqui
 
-# Le a entrada do usuario como uma unica string, por exemplo: "1.60 M"
-entrada = input()
+altura = float(input())
+sexo = input()
 
-# divide a string em duas partes usando espaço como separador:
-# a primeira parte sera a altura (como string), e a segunda o sexo
-altura, sexo = entrada.split() #split divide a string em partes
-
-# converte a altura de string para numero com casas decimais (float)
-altura = float(altura)
-
-# verifica o sexo e aplica a formula correspondente
 if sexo == "M":
     peso = (72.7 * altura) - 58
 else:
     peso = (62.1 * altura) - 44.7
 
-# o :.2f imprime com 2 casas decimais
 print(f"{peso:.2f}")
